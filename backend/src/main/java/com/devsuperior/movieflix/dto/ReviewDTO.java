5e651ada527b70e96a9f4881776a6327c93057bb
GuhTiagoSilva <46786490+GuhTiagoSilva@users.noreply.github.com>
2021-05-12 13:08:26 -0300

    DTOs

---
+++ backend/src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -0,0 +1,91 @@
+package com.devsuperior.movieflix.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.movieflix.entities.Review;
+
+public class ReviewDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String text; 
+	private Long movieId;
+	private Long userId;
+	
+	public ReviewDTO() {
+		
+	}
+
+	public ReviewDTO(Long id, String text, Long movieId, Long userId) {
+		super();
+		this.id = id;
+		this.text = text;
+		this.movieId = movieId;
+		this.userId = userId;
+	}
+	
+	public ReviewDTO(Review entity) {
+		id = entity.getId();
+		text = entity.getText();
+		movieId = entity.getMovie().getId();
+		userId = entity.getUser().getId();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Long getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Long movieId) {
+		this.movieId = movieId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReviewDTO other = (ReviewDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
