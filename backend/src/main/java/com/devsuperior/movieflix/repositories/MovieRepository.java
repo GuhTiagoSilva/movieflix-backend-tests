d69c0f59106dc97d8520368668b6739bd559e011
Gustavo Tiago da Silva <gustavotiagodasilva@MacBook-Pro-de-Gustavo.local>
2021-05-12 17:34:51 -0300

    Resource Server Config, MovieRepository Query (Missing some adjusts)

--- backend/src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ backend/src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -1,11 +1,23 @@
 package com.devsuperior.movieflix.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+	@Query("SELECT m FROM Movie m INNER JOIN m.genre g WHERE g = :genre OR g IS NOT NULL")
+	Page<Movie>find(Genre genre, Pageable pageable);
+	
+	@Query("SELECT m FROM Movie m INNER JOIN m.genre g WHERE g = :genre OR g IS NOT NULL")
+	List<Movie>findAll(Genre genre);
+	
 }
---
+++ backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,50 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+	
+	@Autowired
+	private GenreRepository genreRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAllPaged(PageRequest pageRequest,Long genreId){
+		Genre genre = genreId == 0 ? null : genreRepository.getOne(genreId);
+		Page<Movie> result = repository.find(genre, pageRequest);
+		return result.map(movie -> new MovieDTO(movie));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<MovieDTO> findAll(Long genreId){
+		Genre genre = genreId == 0 ? null : genreRepository.getOne(genreId);
+		List<Movie> result = repository.findAll(genre);
+		return result.stream().map(movie -> new MovieDTO(movie)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Optional<Movie> result = repository.findById(id);
+		Movie movie = result.get();
+		return new MovieDTO(movie);
+	}
+
+	
+}
